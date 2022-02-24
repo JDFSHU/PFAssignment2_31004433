6c47d700411de361a52fff95db368869fccc71d5
JDFSHU <jacob.d.frazer@googlemail.com>
2022-02-24 13:04:31 +0000

    Section D provisionaly completed, needs unit testing to ensure full completion and need to question tutors if they want server.py and client.py files seperate to main program or can they be stand alone files like I have them currently.

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import random
 import string
 import socket
 from threading import Thread
+import os
+
 
 # Name: Jacob Frazer
 # Student Number: 31004433
@@ -17,23 +19,25 @@ def main_menu():  # Main menu function
     print("4: Quit\n")
 
 
-def encrypt(text, s):
-    result = ""
-    for i in range(len(text)):
-        char = text[i]
-        if (char.isupper()):  # Encrypt uppercase characters
-            result += chr((ord(char) + s - 65) % 26 + 65)
-        if (char.isspace()):  # ensuring whitespace / space
-            result += chr((ord(char)))
-        else: # Encrypt lowercase characters
-            result += chr((ord(char) + s - 97) % 26 + 97)
-    return result
+def encrypt_data():  # Section B req 2, 3 & 4
 
+    def encrypt(text, s):  # indented function for returning Caesar cipher encryption
+        result = ""
+        whitespace = "@"  # white space / spaces encrypted as @ symbol
+        for i in range(len(text)):
+            char = text[i]
+            if (char.isupper()):  # Encrypt uppercase characters
+                result += chr((ord(char) + s - 65) % 26 + 65)
+            if (char.isspace()):  # ensuring whitespace / space is included in encryption
+                result += chr((ord(whitespace)))
+            else:  # Encrypt lowercase characters
+                result += chr((ord(char) + s - 97) % 26 + 97)
+        return result
 
-def encrypt_data():
     encrypt_data_loop = True
     while encrypt_data_loop:
-        print("\n:: Encrypt Data ::")
+        print("\n\t :: Encrypt/Decrypt Data ::")
+        print("=" * 42)
         print("1. Encrypt a message and save into a file")
         print("2. Encrypt a file")
         print("3. Decrypt a file")
@@ -43,21 +47,21 @@ def encrypt_data():
         while not encrypt_data_selection.isdigit():  # checking that main menu selection is a digit
             encrypt_data_selection = input("\nYou have entered a non digit value, Select again: ")
 
-        if encrypt_data_selection == "1":
-            message_to_encrypt = input("Input a message to encrypt: ")  # allows user to input a message
+        if encrypt_data_selection == "1":  # req 2
+            message_to_encrypt = input("\nInput a message to encrypt: ")  # allows user to input a message
             print("Your message is ", message_to_encrypt)  # user prompted with their message
             encryption_key = int(input(
                 "Input a number for the encryption key (number should be between 1 and 26 - inclusive: "))
             print("Your encryption key is: ", encryption_key)
             print("Encoded Message: " + encrypt(message_to_encrypt, encryption_key))
             print("\nMessage saved to encoded_msg.txt")
-            with open("encoded_msg.txt", "w") as f:
+            with open("encoded_msg.txt", "w") as f:  # file handing with open allows me to not need a close statement
                 f.write(encrypt(message_to_encrypt, encryption_key))
 
-        elif encrypt_data_selection == "2":
+        elif encrypt_data_selection == "2":  # req 3
             print("option 2 test")
 
-        elif encrypt_data_selection == "3":
+        elif encrypt_data_selection == "3":  # req 4
             print("option 3 test")
 
         elif encrypt_data_selection == "4":
@@ -68,12 +72,63 @@ def encrypt_data():
             print("\nThere is no menu", encrypt_data_selection, "please try again")
 
 
-def extract_data():
-    print("test")
+def extract_data():  # Section C Req's 5, 6, 7, & 8
+    extract_data_loop = True
+    while extract_data_loop:
+        print("\n\t:: Extract Data ::")
+        print("=" * 28)
+        print("1. View all Records")
+        print("2. View Records by Date")
+        print("3. View Records by User")
+        print("4. Count User by Date")
+        print("5. Return to Main Menu\n")
+
+        extract_data_selection = input("Select a menu - Input a number: ")
+        while not extract_data_selection.isdigit():
+            extract_data_selection = input("\nYou have entered a non digit value, Select again: ")
+
+        if extract_data_selection == "1":
+            print("option 1 test")
+
+        elif extract_data_selection == "2":
+            print("option 2 test")
+
+        elif extract_data_selection == "3":
+            print("option 3 test")
+
+        elif extract_data_selection == "4":
+            print("option 4 test")
+
+        elif extract_data_selection == "5":
+            print("\n Exiting to Main Menu")
+            extract_data_loop = False
+        else:
+            print("\nThere is no menu", extract_data_selection, "please try again")
 
 
-def chat_application():
-    print("test")
+def chat_application():  # Section D req 9 & 10
+    chat_application_loop = True
+    while chat_application_loop:
+        print("\n\t :: Chat Application ::")
+        print("=" * 32)
+        print("1. Start Server")
+        print("2. Start Chat (Client)")
+        print("3. Return to Main Menu\n")
+
+        chat_application_selection = input("Select a menu - Input a number: ")
+        while not chat_application_selection.isdigit():
+            chat_application_selection = input("\nYou have entered a non digit value, Select again: ")
+        if chat_application_selection == "1":
+            print("\nStarting Server")
+            os.system("start cmd /k python server.py")
+        elif chat_application_selection == "2":
+            print("\n Opening Chat Client")
+            os.system("start cmd /k python client.py")
+        elif chat_application_selection == "3":
+            print("\n Exiting to Main Menu")
+            chat_application_loop = False
+        else:
+            print("\nThere is no menu", chat_application_selection, "please try again")
 
 
 # This is the main while loop that checks for correct input from the user for the menu and then based on input navigates
