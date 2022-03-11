9c12d63c83d0a831e1618fa4ac9586078575cd4c
JDFSHU <jacob.d.frazer@googlemail.com>
2022-03-11 12:26:31 +0000

    Major gains on encryption, encryption and decryption fully working, currently implementing exception handling for all user inputs.

--- Main.py
+++ Main.py
@@ -18,30 +18,35 @@ def main_menu():  # Main menu function
 
 def encrypt_data():  # Section B req 2, 3 & 4
 
-    def encrypt(text, s):  # indented function for returning Caesar cipher encryption
-        result = ""
-        whitespace = "@"  # white space / spaces encrypted as @ symbol
-        for i in range(len(text)):
-            char = text[i]
-            if (char.isupper()):  # Encrypt uppercase characters
-                result += chr((ord(char) + s - 65) % 26 + 65)
-            if (char.isspace()):  # ensuring whitespace / space is included in encryption
-                result += chr((ord(whitespace)))
-            else:  # Encrypt lowercase characters
-                result += chr((ord(char) + s - 97) % 26 + 97)
+    def encrypt(key, message):
+        message = message.lower()  # converting any message to upper
+        alpha = "abcdefghijklmnopqrstuvwxyz "  # alphabet used to encrypt
+        result = ""  # creating empty variable to store for loop as it iterates
+
+        for letter in message:
+            if letter in alpha:  # if the for loop detects a matching letter in alphabet
+                letter_index = (alpha.find(letter) + key) % len(
+                    alpha)  # finds index positon and adds key, modulo prevents looping around the alpha string
+                result = result + alpha[letter_index]
+            elif letter == " ":  # if letter is whitespace its included in encryption
+                letter_index = (alpha.find(letter) + key) % len(alpha)
+                result = result + alpha[letter_index]
+            else:
+                result = result + letter
         return result
 
-    def decrypt(text, s):  # indented function for returning Caesar cipher encryption
-        result = ""
-        whitespace = " "  # white space / spaces encrypted as @ symbol
-        for i in range(len(text)):
-            char = text[i]
-            if (char.isupper()):  # Encrypt uppercase characters
-                result += chr((ord(char) - s - 65) % 26 + 65)
-            if (char.isspace()):  # ensuring whitespace / space is included in encryption
-                result += chr((ord(whitespace)))
-            else:  # Encrypt lowercase characters
-                result += chr((ord(char) - s - 97) % 26 + 97)
+    def decrypt(key, message):
+        message = message.lower()  # converting any message to upper
+        alpha = "abcdefghijklmnopqrstuvwxyz "  # alphabet used to encrypt
+        result = ""  # creating empty variable to store for loop as it iterates
+
+        for letter in message:
+            if letter in alpha:  # if the for loop detects a matching letter in alphabet
+                letter_index = (alpha.find(letter) - key) % len(
+                    alpha)  # finds index position and adds key, modulo prevents looping around the alpha string
+                result = result + alpha[letter_index]
+            else:
+                result = result + letter
         return result
 
     encrypt_data_loop = True
@@ -60,30 +65,60 @@ def encrypt_data():  # Section B req 2, 3 & 4
         if encrypt_data_selection == "1":  # req 2
             message_to_encrypt = input("\nInput a message to encrypt: ")  # allows user to input a message
             print("Your message is ", message_to_encrypt)  # user prompted with their message
-            encryption_key = int(input(
-                "\nInput a number for the encryption key (number should be between 1 and 26 - inclusive: "))
-            print("Your encryption key is: ", encryption_key)
-            print("Encoded Message: " + encrypt(message_to_encrypt, encryption_key))
-            print("\nMessage saved to encoded_msg.txt")
-            with open("encoded_msg.txt", "w") as f:  # file handing with open allows me to not need a close statement
-                f.write(encrypt(message_to_encrypt, encryption_key))
+            while True:
+                try:
+                    encryption_key = int(input(
+                        "\nInput a number for the encryption key between 1 and 27: "))
+                    while encryption_key < 1 or encryption_key > 27:
+                        encryption_key = int(input(
+                            "\nYou're key is out of bounds, please re enter a key between 1 and 27: "))
+
+                    print("Your encryption key is: ", encryption_key)
+                    print("Encoded Message: " + encrypt(encryption_key, message_to_encrypt))
+                    print("\nMessage saved to encoded_msg.txt")
+                    with open("encoded_msg.txt",
+                              "w") as f:  # file handing with open allows me to not need a close statement
+                        f.write(encrypt(encryption_key, message_to_encrypt))
+                    break
+                except ValueError:
+                    print("You must enter a NUMBER between 1 and 27")
 
         elif encrypt_data_selection == "2":  # req 3
-            file_input = input("Please input the file name to encrypt (format filename.txt): ")
+            file_input = input("\nPlease input the file name to encrypt (format filename.txt): ")
+            print()
             with open(file_input) as file_data:
                 lines = file_data.read().strip()
                 print(lines)
             file_encryption_key = int(input("\nPlease enter the key to encrypt the message: "))
+            while file_encryption_key < 1 or file_encryption_key > 27:
+                file_encryption_key = int(input(
+                    "\nYou're key is out of bounds, please re enter a key between 1 and 27: "))
             with open("encryptedFile.txt",
                       "w") as new_encrypted_file:  # file handing with open allows me to not need a close statement
-                new_encrypted_file.write(encrypt(lines, file_encryption_key))
-            print("Encryption applied, file saved as encryptedFile.txt ")
+                new_encrypted_file.write(encrypt(file_encryption_key, lines))
+            print("\nEncryption applied, file saved as encryptedFile.txt ")
+            print()
             with open("encryptedFile.txt", "r") as show_file:
                 lines = show_file.read()
                 print(lines)
 
         elif encrypt_data_selection == "3":  # req 4
-            print("option 3 test")
+            file_input = input("\nPlease input the file name to decrypt (format filename.txt): ")
+            print()
+            with open(file_input) as file_data:
+                lines = file_data.read().strip()
+                print(lines)
+            file_decryption_key = int(input("\nInput a number for the decryption key between 1 and 27: "))
+            while file_decryption_key < 1 or file_decryption_key > 27:
+                file_decryption_key = int(input(
+                    "\nYou're key is out of bounds, please re enter a key between 1 and 27: "))
+            with open("encryptedFile.txt",
+                      "w") as new_encrypted_file:  # file handing with open allows me to not need a close statement
+                new_encrypted_file.write(decrypt(file_decryption_key, lines))
+            print("\nDecryption applied, file saved as encryptedFile.txt \n")
+            with open("encryptedFile.txt", "r") as show_file:
+                lines = show_file.read()
+                print(lines)
 
         elif encrypt_data_selection == "4":
             print("\n Exiting to Main Menu")
